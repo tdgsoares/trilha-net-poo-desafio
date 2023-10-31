63531b35c3d65b07cb9985be38e6aec43dfece72
TDGS <taisa.tdgs@gmail.com>
2023-10-31 00:26:30 -0300

    Resolução desafio Celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone Nokia = new Nokia(numero: "2323232", modelo: "D20",imei: "5555", memoria: 64);
+
+Nokia.Ligar();
+Nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Smartphone Iphone = new Iphone(numero: "4242424", modelo: "11",imei: "111", memoria: 128);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Telegram");
+
